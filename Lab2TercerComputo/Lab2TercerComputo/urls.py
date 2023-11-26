c27e28a2c6b59ff4c81918a6121c318456d9efcc
Margarita <sofiamargaritagrn@gmail.com>
2023-11-26 15:50:51 -0600

    Actualización de guía

--- Lab2TercerComputo/Lab2TercerComputo/urls.py
+++ Lab2TercerComputo/Lab2TercerComputo/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from GestorApp import views as ap1v
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', ap1v.index,name="home"),
+    path('registro/', ap1v.reg_user),
+    path('login/', ap1v.iniciar_sesion,name="login"),
 ]
